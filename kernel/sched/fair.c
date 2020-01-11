4e4a934670ecf5ee820cb4189406907391a93445
Satya Durga Srinivasu Prabhala <satyap@codeaurora.org>
2020-01-11 06:45:46 -0500

    sched/fair: Prevent false-positive lockdep warnings
    Below warnings are observed during boot when lockdep is enabled
    due to double_lock_balance usage. Fix the warnings by disabling lockdep
    checks before double_lock_balance call and enable it after
    double_unlock_balance call.
    
    [    0.180233] ------------[ cut here ]------------
    [    0.180241] releasing a pinned lock
    [    0.180271] WARNING: CPU: 0 PID: 17 at kernel/locking/lockdep.c:3676 \
    						lock_release+0x30c/0x328
    [    0.181279] Call trace:
    [    0.181286]  lock_release+0x30c/0x328
    [    0.181298]  _raw_spin_unlock+0x28/0x60
    [    0.181307]  detach_task+0x74/0x130
    [    0.181316]  active_load_balance_cpu_stop+0x48c/0x520
    [    0.181327]  cpu_stopper_thread+0xac/0x150
    [    0.181337]  smpboot_thread_fn+0x1c8/0x2e8
    [    0.181346]  kthread+0x11c/0x130
    [    0.181356]  ret_from_fork+0x10/0x1c
    [    0.181447] ------------[ cut here ]------------
    [    0.181453] unpinning an unpinned lock
    [    0.181467] WARNING: CPU: 0 PID: 17 at kernel/locking/lockdep.c:3791 \
    						lock_unpin_lock+0xcc/0xf0
    [    0.182453] Call trace:
    [    0.182460]  lock_unpin_lock+0xcc/0xf0
    [    0.182468]  active_load_balance_cpu_stop+0x288/0x520
    [    0.182475]  cpu_stopper_thread+0xac/0x150
    [    0.182482]  smpboot_thread_fn+0x1c8/0x2e8
    [    0.182488]  kthread+0x11c/0x130
    [    0.182495]  ret_from_fork+0x10/0x1c.
    
    Change-Id: I8113e7f7d01d2e84609e35563b507aea90236e70
    Signed-off-by: Satya Durga Srinivasu Prabhala <satyap@codeaurora.org>
    Signed-off-by: Adam W. Willis <return.of.octobot@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -9281,6 +9281,7 @@ static void detach_task(struct task_struct *p, struct lb_env *env)
 
 	p->on_rq = TASK_ON_RQ_MIGRATING;
 	deactivate_task(env->src_rq, p, DEQUEUE_NOCLOCK);
+	lockdep_off();
 #ifdef CONFIG_SCHED_WALT
 	double_lock_balance(env->src_rq, env->dst_rq);
 	if (!(env->src_rq->clock_update_flags & RQCF_UPDATED))
@@ -9290,6 +9291,7 @@ static void detach_task(struct task_struct *p, struct lb_env *env)
 #else
 	set_task_cpu(p, env->dst_cpu);
 #endif
+	lockdep_on();
 }
 
 /*
