be27e1322ebb58abb2ac9fc4bb6bf843f7e14e80
Althaf Neelanchirayil <aneelanc@codeaurora.org>
2021-05-28 17:24:29 +0530

    fbdev: msm: Avoid race condition while iommu mapping from DSI flow
    Avoid race condition which occurs when SMMU get detached from
    secure display flow and the iommu mapping happening from
    ESD (DSI reg read) flow.
    
    Change-Id: Ief6e9cf09369dc2f1381c77fb59f35648dbbb2e0
    Signed-off-by: Althaf Neelanchirayil <aneelanc@codeaurora.org>
    Also update source code header
    Signed-off-by: ederekun <sedrickvince@gmail.com>

--- drivers/video/fbdev/msm/dsi_status_6g.c
+++ drivers/video/fbdev/msm/dsi_status_6g.c
@@ -1,14 +1,5 @@
-/* Copyright (c) 2013-2018, 2020, The Linux Foundation. All rights reserved.
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License version 2 and
- * only version 2 as published by the Free Software Foundation.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- */
+// SPDX-License-Identifier: GPL-2.0-only
+/* Copyright (c) 2013-2018, 2020-2021, The Linux Foundation. All rights reserved. */
 
 #include <linux/workqueue.h>
 #include <linux/delay.h>
@@ -161,7 +152,9 @@ void mdss_check_dsi_ctrl_status(struct work_struct *work, uint32_t interval)
 	MDSS_XLOG(mipi->mode);
 
 	mdss_mdp_clk_ctrl(MDP_BLOCK_POWER_ON);
+	mutex_lock(&pstatus_data->mfd->sd_lock);
 	ret = ctrl_pdata->check_status(ctrl_pdata);
+	mutex_unlock(&pstatus_data->mfd->sd_lock);
 	mdss_mdp_clk_ctrl(MDP_BLOCK_POWER_OFF);
 
 	mutex_unlock(&ctl->offlock);
