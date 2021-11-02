2dc9f3b4853f6e32b8ba6039568210726c670e2f
Roberto Sartori <roberto.sartori.android@gmail.com>
2021-11-02 19:04:44 +0100

    input: fingerprint: goodix: actually adapt to kernel 4.14
    Follow Linaro documentation to adapt to the new 'suspend blockers' on
    kernel 4.14:
    https://www.linaro.org/blog/converting-code-implementing-suspend-blockers/
    
    Signed-off-by: Roberto Sartori <roberto.sartori.android@gmail.com>

--- drivers/input/fingerprint/goodix/gf_spi.c
+++ drivers/input/fingerprint/goodix/gf_spi.c
@@ -73,7 +73,7 @@ static int SPIDEV_MAJOR;
 static DECLARE_BITMAP(minors, N_SPI_MINORS);
 static LIST_HEAD(device_list);
 static DEFINE_MUTEX(device_list_lock);
-//static struct wake_lock fp_wakelock;
+static struct wakeup_source fp_wakelock;
 static struct gf_dev gf;
 
 struct gf_key_map maps[] = {
@@ -493,7 +493,7 @@ static irqreturn_t gf_irq(int irq, void *handle)
 #if defined(GF_NETLINK_ENABLE)
 	char temp = GF_NET_EVENT_IRQ;
 
-	//wake_lock_timeout(&fp_wakelock, msecs_to_jiffies(WAKELOCK_HOLD_TIME));
+	__pm_wakeup_event(&fp_wakelock, msecs_to_jiffies(WAKELOCK_HOLD_TIME));
 	sendnlmsg(&temp);
 #elif defined GF_FASYNC
 	struct gf_dev *gf_dev = &gf;
@@ -792,7 +792,7 @@ static int gf_probe(struct platform_device *pdev)
 
 	gf_dev->irq = gf_irq_num(gf_dev);
 
-	//wake_lock_init(&fp_wakelock, WAKE_LOCK_SUSPEND, "fp_wakelock");
+	wakeup_source_init(&fp_wakelock, "fp_wakelock");
 	status = request_threaded_irq(gf_dev->irq, NULL, gf_irq,
 			IRQF_TRIGGER_RISING | IRQF_ONESHOT,
 			"gf_fp", gf_dev);
@@ -853,7 +853,7 @@ static int gf_remove(struct platform_device *pdev)
 {
 	struct gf_dev *gf_dev = &gf;
 
-	//wake_lock_destroy(&fp_wakelock);
+	wakeup_source_trash(&fp_wakelock);
 	/* make sure ops on existing fds can abort cleanly */
 	if (gf_dev->irq)
 		free_irq(gf_dev->irq, gf_dev);
