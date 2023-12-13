f296eae7d581d02a5e3bb328901784c4e9a3795e
anonymous <f1aa01e81ea4@mail.clos>
2023-12-13 01:37:42 +0000

    msm: mdss: kcal: Add KCAL support

--- drivers/video/fbdev/msm/mdss_mdp_pp_cache_config.c
+++ drivers/video/fbdev/msm/mdss_mdp_pp_cache_config.c
@@ -1199,9 +1199,15 @@ static int pp_pa_cache_params_v1_7(struct mdp_pa_v2_cfg_data *config,
 
 	if (copy_from_user(&pa_usr_config, config->cfg_payload,
 			   sizeof(pa_usr_config))) {
+#ifdef CONFIG_FB_MSM_MDSS_KCAL_CTRL
+		pr_err("%s Failed to copy v1_7 PA - copying in kernel \n",__func__);
+		memcpy(&pa_usr_config, config->cfg_payload, sizeof(pa_usr_config));
+		ret = 0;
+#else
 		pr_err("Failed to copy v1_7 PA\n");
 		ret = -EFAULT;
 		goto pa_config_exit;
+#endif
 	}
 
 	if ((config->flags & MDP_PP_OPS_DISABLE)) {
