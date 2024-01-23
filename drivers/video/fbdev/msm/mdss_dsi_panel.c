5f17fb432d0233b22823ebe2a10267fb55940146
anonymous <f1aa01e81ea4@mail.clos>
2024-01-23 08:12:50 +0000

    Revert "msm: mdss: do not allow backlight to change when DSI is in low power mode"
    This reverts commit ab440848691c09f0d99684c2ce4354acb5e6522c.

--- drivers/video/fbdev/msm/mdss_dsi_panel.c
+++ drivers/video/fbdev/msm/mdss_dsi_panel.c
@@ -924,12 +924,6 @@ static void mdss_dsi_panel_bl_ctrl(struct mdss_panel_data *pdata,
 	if (pdata->panel_disable_mode && (bl_level != 0))
 		return;
 
-	/* do not allow backlight to change when DSI is in low power mode */
-	if (mdss_dsi_is_panel_on_lp(pdata)) {
-		pr_debug("Cannot control brightness in DSI low power mode");
-		return;
-	}
-
 	if ((bl_level < pdata->panel_info.bl_min) && (bl_level != 0))
 		bl_level = pdata->panel_info.bl_min;
 
